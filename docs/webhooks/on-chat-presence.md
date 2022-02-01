7f0e4208f26fdaeab206ede6229530c8dc9d2998
gmsanjuliano <gmsanjuliano@gmail.com>
2022-02-01 08:05:49 -0300

    first clone

---
+++ docs/webhooks/on-chat-presence.md
@@ -0,0 +1,150 @@
+---
+id: on-chat-presence
+title: Status do chat
+---
+
+## Método
+
+#### `POST` on-chat-presence
+
+## Conceituação
+
+Esse é o webhook de retorno status do chat
+
+:::caution Atenção
+
+O Z-API não aceita webhooks que não sejam HTTPS
+
+:::
+
+---
+
+## Atualizando Webhook
+
+Para atualizar a rota do webhook é possível fazer isso pela API ou pelo painel administrativo.
+
+### API
+
+#### /update-webhook-chat-presence
+
+`PUT` https://api.z-api.io/instances/SUA_INSTANCIA/token/SEU_TOKEN/update-webhook-chat-presence
+
+#### Request Body
+
+#### Body
+
+```json
+{
+  "value": "https://endereco-do-seu-sistema.com.br/instancia/SUA_INSTANCIA/presence"
+}
+```
+
+---
+
+### Painel Administrativo
+
+![img](../../img/chatPresence.png)
+
+---
+
+## Atributos
+
+### Obrigatórios
+
+| Atributos | Tipo | Descrição |
+| :-- | :-: | :-- |
+| type | string | Tipo do evento da instância, nesse caso será "DeliveryCallback". |
+| phone | string | Número de telefone de destino da mensagem. |
+| status | string | Identificador do status do chat ex: (Digitando...) status pode conter ( UNAVAILABLE, AVAILABLE, COMPOSING, RECORDING) |
+| lastSeen | timestamp | Identificador da ultima vez presente do usuário. |
+
+---
+
+## Response
+
+### 200
+
+### Fora do chat
+
+```json
+{
+  "type": "PresenceChatCallback",
+  "phone": "5544999999999",
+  "status": "UNAVAILABLE",
+  "lastSeen": null
+}
+```
+
+### Dentro do chat
+
+```json
+{
+  "type": "PresenceChatCallback",
+  "phone": "5544999999999",
+  "status": "AVAILABLE",
+  "lastSeen": null
+}
+```
+
+### Digitando no chat
+
+```json
+{
+  "type": "PresenceChatCallback",
+  "phone": "5544999999999",
+  "status": "COMPOSING",
+  "lastSeen": null
+}
+```
+
+### Parou de digitar ou apagou o que estava digitando
+
+```json
+{
+  "type": "PresenceChatCallback",
+  "phone": "5544999999999",
+  "status": "PAUSED",
+  "lastSeen": null
+}
+```
+
+:::tip Aviso
+
+**Observação:**
+
+Após receber um composing ou um recording, um **PAUSED** será retornado quando o evento parar
+
+O stauts **PAUSED** apenas é retornado se estiver usando o beta multi-devices
+
+:::
+
+### Gravando áudio no chat
+
+```json
+{
+  "type": "PresenceChatCallback",
+  "phone": "5544999999999",
+  "status": "RECORDING",
+  "lastSeen": null
+}
+```
+
+:::tip Aviso
+
+O stauts **RECORDING** apenas é retornado se estiver usando o beta multi-devices
+
+:::
+
+### 405
+
+Neste caso certifique que esteja enviando o corretamente a especificação do método, ou seja verifique se você enviou o POST ou GET conforme especificado no inicio deste tópico.
+
+### 415
+
+Caso você receba um erro 415, certifique de adicionar na headers da requisição o "Content-Type" do objeto que você está enviando, em sua grande maioria "application/json"
+
+<!-- ---
+
+## Code
+
+<iframe src="//api.apiembed.com/?source=https://raw.githubusercontent.com/Z-API/z-api-docs/main/json-examples/on-chat-presence.json&targets=all" frameborder="0" scrolling="no" width="100%" height="500px" seamless></iframe> -->
---
+++ sidebars.js
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+module.exports = {
+  tutorialSidebar: {
+    'Quick Start': ['quickstart/introduction', 'quickstart/authentication'],
+
+    Dicas: [
+      'tips/blockednumber',
+      'tips/enable-fix-number',
+      'tips/disable-screem-blocked',
+      'tips/android-active',
+      'tips/stability',
+      'tips/emulators',
+      'tips/Z-APIvsAPI-OFICIAL',
+    ],
+
+    Instância: [
+      'instance/introduction',
+      'instance/qrcode',
+      'instance/restore-session',
+      'instance/restart',
+      'instance/disconnect',
+      'instance/status',
+      'instance/device',
+    ],
+    Mensagens: [
+      'message/introduction',
+      'message/send-message-text',
+      'message/send-message-image',
+      'message/send-message-sticker',
+      'message/send-message-audio',
+      'message/send-message-video',
+      'message/send-message-document',
+      'message/send-message-link',
+      'message/send-message-location',
+      /* 'send-message/send-message-product',
+      'send-message/send-message-catalog',*/
+      'message/send-message-contact',
+      'message/send-button-list',
+      'message/send-option-list',
+      'message/delete-message',
+      'message/read-message',
+      'message/reply-message',
+    ],
+    Contatos: [
+      'contacts/introduction',
+      'contacts/get-contacts',
+      'contacts/get-metadata-contact',
+      'contacts/get-profile-picture',
+      'contacts/get-iswhatsapp',
+    ],
+    Chats: [
+      'chats/introduction',
+      'chats/get-chats',
+      'chats/get-message-chats',
+      'chats/get-metadata-chat',
+      'chats/archive-chat',
+      'chats/pin-chat',
+      'chats/delete-chat',
+    ],
+    Grupos: [
+      'group/introduction',
+      'group/create-group',
+      'group/update-group-name',
+      'group/update-group-photo',
+      'group/add-participant',
+      'group/remove-participant',
+      'group/add-admin',
+      'group/remove-admin',
+      'group/leave-group',
+      'group/metadata-group',
+      'group/group-invitation-metadata',
+      'group/update-group-settings',
+      'group/update-group-description',
+    ],
+    'Lista de Transmissão': ['broadcast/introduction'],
+    Status: [
+      'status/introduction',
+      'status/send-text-status',
+      'status/send-image-status',
+    ],
+    'Fila de Mensagens': [
+      'queue/introduction',
+      'queue/get-queue',
+      'queue/delete-queue',
+      'queue/delete-queue-id',
+    ],
+    'Whatsapp Business': [
+      'business/introduction',
+      'business/edit-product',
+      'business/get-products',
+      'business/get-products-phone',
+      'business/get-product-id',
+      'business/delete-product',
+    ],
+    Webhooks: [
+      'webhooks/introduction',
+      'webhooks/on-message-send',
+      'webhooks/on-message-received',
+      'webhooks/on-whatsapp-disconnected',
+      'webhooks/on-whatsapp-message-status-changes',
+      'webhooks/on-chat-presence',
+    ],
+    Parceiros: [
+      'partner/introduction',
+      'partner/create-instance',
+      'partner/sign-instance',
+      'partner/unsubscribe-instance',
+    ],
+
+    Integradores: ['integrators/find-my-pack'],
+
+    'Multi-Devices': ['multidevices/introduction', 'multidevices/beta'],
+
+    Sample: ['sample/exemplos'],
+  },
+};
